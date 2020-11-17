9e1a5940c25056631e945f34a2aeeacb0b1e7a2c
Maximilian Schwarzmüller <maximilianschwarzmuller@Max-MBP.fritz.box>
2020-11-17 11:41:49 +0100

    added code

---
+++ function-order.js
@@ -0,0 +1,15 @@
+start();
+
+function start() {
+  console.log('start');
+  next();
+}
+
+function next() {
+  console.log('next');
+  last();
+}
+
+function last() {
+  console.log('last');
+}
---
+++ function-order.py
@@ -0,0 +1,23 @@
+# Won't work
+# start()
+
+
+def start():
+    print('Start')
+    next()
+
+
+# Won't work
+# start()
+
+
+def next():
+    print('Next')
+    last()
+
+
+def last():
+    print('Last')
+
+# Will work
+start()
